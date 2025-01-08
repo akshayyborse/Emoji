f3aa932e5b65378964a1b186b30afb5fe267d6d5
akshayyborse <borseakshay128@gmail.com>
2025-01-08 19:21:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import EmojiGallery from './components/EmojiGallery';
 import Logo from './components/Logo';
 import './App.css';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
